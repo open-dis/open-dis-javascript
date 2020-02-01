b0c1c9949fbc0c40cf0a409a618799263ef3ba8b
Leif Gruenwoldt <leifer@gmail.com>
2020-01-31 22:51:48 -0500

    Remove old patches
    These have been applied and are not required anymore.

--- patches/applyPatchesDis6.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-# Apply patches to the generated code
-
-# Marking.java, apply patch file to ensure character fields are exactly 11 bytes long after set operations
-patch javascript/dis/Marking.js patches/Marking.diff
-
--- patches/applyPatchesDis7.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-# Apply patches to the generated code
-
-# Marking.java, apply patch file to ensure character fields are exactly 11 bytes long after set operations
-
-patch javascript/dis7/EntityMarking.js patches/EntityMarking.diff
